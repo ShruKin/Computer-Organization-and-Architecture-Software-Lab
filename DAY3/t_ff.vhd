72c977c35042ffe39a5ca053e85dcb8986f40d2a
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-04-20 00:43:53 +0530

    Fixed T-flip flop

--- DAY3/t_ff.vhd
+++ DAY3/t_ff.vhd
@@ -3,23 +3,28 @@ USE ieee.std_logic_1164.ALL;
 
 ENTITY t_ff IS
     PORT (
-        T, CLOCK : IN std_logic;
+        T, CLOCK, SET : IN std_logic;
         Q, QBAR : OUT std_logic);
 END t_ff;
 
 ARCHITECTURE bhv OF t_ff IS
-    SIGNAL tmp : std_logic;
 
 BEGIN
-    PROCESS (CLOCK)
+
+    PROCESS (CLOCK, SET)
+        VARIABLE tmp : std_logic;
     BEGIN
         IF (CLOCK = '1' AND CLOCK'EVENT) THEN
-            IF (T = '0') THEN
-                tmp <= tmp;
+            IF (SET = '1') THEN
+                tmp := '1';
+            ELSIF (T = '0') THEN
+                tmp := tmp;
             ELSE
-                tmp <= NOT tmp;
+                tmp := NOT tmp;
             END IF;
         END IF;
+
+        Q <= tmp;
+        Qbar <= NOT tmp;
     END PROCESS;
-    Q <= tmp;
 END bhv;
